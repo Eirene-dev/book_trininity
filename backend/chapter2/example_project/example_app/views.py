71844170ba15df7a7a452e30ad4cc527f1cdd5b3
Eirene <zu007kr@gmail.com>
2023-05-28 21:03:52 +0900

    Initial commit

---
+++ backend/chapter2/example_project/example_app/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+
+def hello(request):
+   return HttpResponse("Hello, World!")
+
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+
+@api_view(['GET', 'POST'])
+@permission_classes([AllowAny])
+def hello_rest_api(request):
+    data = {'message': 'Hello, REST API!'}
+    return Response(data)
+
---
+++ frontend/chapter2/my-react-app/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const IndexPage = () => {
+  return (
+    <div>
+      <h1>Welcome to Next.js!</h1>
+    </div>
+  );
+};
+
+export default IndexPage;
